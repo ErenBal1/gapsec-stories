ee791f27445379a9983a8c5e9242da48177965d7
Taha Yasin Bike <tahayasin.bike@hotmail.com>
2024-09-11 01:48:52 +0300

    "Updated version code, AndroidManifest.xml, Info.plist, and ad unit IDs in adMobService and shop_view."

--- lib/adMobService/ad_mob_service.dart
+++ lib/adMobService/ad_mob_service.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdMobService {
   static String? get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3311093782197009/7196430018";
+      return "ca-app-pub-3311093782197009/4432898821";
     } else if (Platform.isIOS) {
-      return "ca-app-pub-3311093782197009/7827797477";
+      return "ca-app-pub-3311093782197009/5720572524";
       //ca-app-pub-3940256099942544/5224354917
     }
     return null;
